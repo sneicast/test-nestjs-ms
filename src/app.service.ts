7d6ae4659ab9b276598041e69976a75eca4492d5
Sneider Castillo <sneiderz15@gmail.com>
2025-12-16 05:36:34 -0500

    variables de entorno

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   getHello(): string {
-    return 'Nueva prueba';
+    const environment = this.configService.get<string>('ENVIROMENT', 'development');
+    return `Hello from ${environment}`;
   }
 }
